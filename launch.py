3c3b5011f9a303a4ffaeeeac670e8e032e39d705
agiveon <agiveon@gmail.com>
2021-09-12 20:42:25 -0700

    Update launch.py

--- launch.py
+++ launch.py
@@ -1,7 +1,7 @@
 import gradio
 
-def hello(inp):
-  return "Hello {}!".format(inp)
+def hello(YOUR_NAME):
+  return "Hello {}!".format(YOUR_NAME)
 
 io = gradio.Interface(fn=hello, inputs='text', outputs='text', title='Hello World', 
     description='My first hosted interface!',share=True)  
